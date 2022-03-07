51357c4906a355420e501e773460029ad36193a4
Irishika <100313582+Irishika@users.noreply.github.com>
2022-03-07 21:39:10 +0300

    b

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,19 +34,17 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client:'', goods: [], order: {}};
+    let client = `${name} ${phone}`;
     
-    data.client = name+ ' ' + phone;
-
-    let order = {address: '', sum: 0};
-    order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
-    order.sum = sum;
-    data.order = order;
-
+    let order = {address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`, sum: sum};
+    
+    let g = [];
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i ++) {
-        data.goods.push({title: goods[i].title, count: goods[i].count});
+        g.push({title: goods[i].title, count: goods[i].count});
     };
+    
+    let data = {client: client, order: order, goods: g};
 
     let jsonData = JSON.stringify(data);
 
